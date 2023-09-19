bebf41c0ccce462758648a9b2d932fdca2807fad
blaec <odeskonst@yandex.ru>
2023-09-19 23:29:00 +0300

    #6 display album

--- src/Pages/Memories.js
+++ src/Pages/Memories.js
@@ -1,10 +1,41 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+
+import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
+
 
 const Memories = () => {
+    const {albums, isAlbumsLoaded} = useSelector(state => state.album.albums);
+
+    let link = isAlbumsLoaded ? (
+        <a href={albums[1].album} target="_blank">
+            <img
+                src={albums[1].image}
+                alt=""
+                width="200"
+                onError={(e) => {
+                    e.target.onerror = null;
+                    e.target.src = ""
+                }}
+            />
+        </a>
+    ) : null;
+
     return (
-        <div>
-            memories
-        </div>
+        <Card sx={{maxWidth: 345}}>
+            <CardActionArea>
+                {link}
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     );
 };
 
