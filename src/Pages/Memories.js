0febad8f5936677cedb16e8e0c7efbe5daecf116
blaec <odeskonst@yandex.ru>
2023-09-20 18:43:04 +0300

    #6 display album using json-db

--- src/Pages/Memories.js
+++ src/Pages/Memories.js
@@ -7,36 +7,37 @@ import {Card, CardActionArea, CardContent, Typography} from "@mui/material";
 const Memories = () => {
     const {albums, isAlbumsLoaded} = useSelector(state => state.album.albums);
 
-    let link = isAlbumsLoaded ? (
-        <a href={albums[1].album} target="_blank">
-            <img
-                src={albums[1].image}
-                alt=""
-                width="200"
-                onError={(e) => {
-                    e.target.onerror = null;
-                    e.target.src = ""
-                }}
-            />
-        </a>
-    ) : null;
+    return isAlbumsLoaded
+        ? albums.map((album) =>
+            (<Card sx={{maxWidth: 345}}>
+                    <CardActionArea>
+                        <a href={album.album}
+                           target="_blank"
+                           rel="noreferrer"
+                        >
+                            <img
+                                src={album.image}
+                                alt=""
+                                style={{width: '350px', height: '200px', 'object-fit': 'cover'}}
+                                onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src = ""
+                                }}
+                            />
+                        </a>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {album.title}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                {album.description}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
 
-    return (
-        <Card sx={{maxWidth: 345}}>
-            <CardActionArea>
-                {link}
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    );
+            ))
+        : null;
 };
 
 export default Memories;
