cd262752cbe146a777456673f97c1050dcf65068
blaec <odeskonst@yandex.ru>
2023-09-24 00:11:06 +0300

    #6 refactoring

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ const App = () => {
 
     useEffect(() => {
         dispatch(fetchAlbums());
-    }, []);
+    }, [dispatch]);
 
 
     return (
@@ -38,37 +38,6 @@ const App = () => {
             {layout}
         </>
     );
-
-    // const [posts, setPosts] = useState([]);
-    //
-    // const getData = () => {
-    //     var requestOptions = {
-    //         method: "GET",
-    //         redirect: "follow",
-    //     };
-    //
-    //     fetch("http://localhost:3033/posts", requestOptions)
-    //         .then((response) => response.json())
-    //         .then((result) => setPosts(result))
-    //         .catch((error) => console.log("error", error));
-    // };
-    //
-    // useEffect(() => {
-    //     getData();
-    // }, []);
-    //
-    // return (
-    //     <div>
-    //         {posts.map((post) => (
-    //             <div key={post.id}>
-    //                 <h3>
-    //                     <span>{post.id}</span> {post.title}
-    //                 </h3>
-    //                 <p>{post.body}</p>
-    //             </div>
-    //         ))}
-    //     </div>
-    // );
 }
 
 export default App;
