442915cc73cf37458c1321fe2d52a370210b4f9f
blaec <odeskonst@yandex.ru>
2023-09-24 00:03:45 +0300

    #6 refactoring

--- src/axios-album.js
+++ src/axios-album.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3033/posts'
+    baseURL: 'http://10.100.102.4:3033/posts'
 });
 
 export default instance;
