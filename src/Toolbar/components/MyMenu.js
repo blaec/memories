aec29cb02f288726c121b1bc23fd4cf0c8a38772
blaec <odeskonst@yandex.ru>
2023-09-24 11:43:20 +0300

    #7 create multilevel menu

--- src/Toolbar/components/MyMenu.js
+++ src/Toolbar/components/MyMenu.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import MyListItem from "../../UI/MyListItem";
 import {reactLinks} from "../../Utils/UriUtils";
 
-import {Divider, List, Toolbar} from "@mui/material";
+import {Collapse, Divider, List, ListItemButton, ListItemIcon, ListItemText, Toolbar} from "@mui/material";
 import VpnKeyTwoToneIcon from '@mui/icons-material/VpnKeyTwoTone';
 import SettingsTwoToneIcon from '@mui/icons-material/SettingsTwoTone';
+import EditNoteTwoToneIcon from '@mui/icons-material/EditNoteTwoTone';
+import LuggageTwoToneIcon from '@mui/icons-material/LuggageTwoTone';
+import ConnectingAirportsTwoToneIcon from '@mui/icons-material/ConnectingAirportsTwoTone';
+import PhotoSizeSelectActualTwoToneIcon from '@mui/icons-material/PhotoSizeSelectActualTwoTone';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import ExpandLess from '@mui/icons-material/ExpandLess';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import StarBorder from '@mui/icons-material/StarBorder';
+import MyCollapseListItem from "../../UI/MyCollapseListItem";
+import ArrowRightTwoToneIcon from '@mui/icons-material/ArrowRightTwoTone';
 
 
 const getMenuItemsBlock = (itemsData) => {
-
     return  itemsData.map((item, index) => {
-            const {text, link, icon} = item;
+            const {text, link, icon, style} = item;
 
             return (
                 <MyListItem
@@ -19,6 +28,7 @@ const getMenuItemsBlock = (itemsData) => {
                     caption={text}
                     link={link}
                     icon={icon}
+                    style={style}
                 />
             );
         }
@@ -27,27 +37,73 @@ const getMenuItemsBlock = (itemsData) => {
 
 const mainItemsData = [
     {
-        text: "Passwords",
+        text: "Воспоминания",
         link: reactLinks.memories,
-        icon: <VpnKeyTwoToneIcon/>
+        icon: <EditNoteTwoToneIcon/>
     },
 ];
-const settingsItemsData = [
+const guestItemsData = [
     {
-        text: "Settings",
+        text: "В Израиле",
         link: reactLinks.settings,
-        icon: <SettingsTwoToneIcon/>
+        icon: <ArrowRightTwoToneIcon/>,
+        style: {pl: 4}
+    },
+    {
+        text: "В Кременчуге",
+        link: reactLinks.settings,
+        icon: <ArrowRightTwoToneIcon/>,
+        style: {pl: 4}
+    },
+];
+const travelItemsData = [
+    {
+        text: "Израиля",
+        link: reactLinks.settings,
+        icon: <ArrowRightTwoToneIcon/>,
+        style: {pl: 4}
+    },
+    {
+        text: "Кременчуга",
+        link: reactLinks.settings,
+        icon: <ArrowRightTwoToneIcon/>,
+        style: {pl: 4}
+    },
+    {
+        text: "других стран",
+        link: reactLinks.settings,
+        icon: <ArrowRightTwoToneIcon/>,
+        style: {pl: 4}
+    },
+];
+const otherItemsData = [
+    {
+        text: "Разное",
+        link: reactLinks.settings,
+        icon: <PhotoSizeSelectActualTwoToneIcon/>
     },
 ];
 
+
 const MyMenu = () => {
+
     return (
         <div>
             <Toolbar/>
             <List>
                 {getMenuItemsBlock(mainItemsData)}
-                <Divider/>
-                {getMenuItemsBlock(settingsItemsData)}
+                <MyCollapseListItem
+                    caption="В гостях"
+                    icon=<LuggageTwoToneIcon/>
+                    menuBlock={() => getMenuItemsBlock(guestItemsData)}
+                />
+                <MyCollapseListItem
+                    caption="Фото из"
+                    icon=<ConnectingAirportsTwoToneIcon/>
+                    menuBlock={() => getMenuItemsBlock(travelItemsData)}
+                />
+                {getMenuItemsBlock(otherItemsData)}
+                {/*<Divider/>*/}
             </List>
         </div>
     )
