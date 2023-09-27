e108dc42e9b7cc476f2bfe5b928282f720a1ba9d
blaec <odeskonst@yandex.ru>
2023-09-27 23:44:25 +0300

    #11 last instructions

--- src/Pages/Instructions/Instructions.js
+++ src/Pages/Instructions/Instructions.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import TextBlock from "./components/TextBlock";
-
-import {Box, Grid, Typography} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import Accordion from '@mui/material/Accordion';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import AccordionSummary from '@mui/material/AccordionSummary';
@@ -95,7 +93,7 @@ const Instructions = () => {
                     <Typography>
                         Для того, чтобы открыть ролик вне сайта - нужно щелкнуть по заголовку ролика как это показано ниже.
                     </Typography>
-                    <img src="/images/open_video_outside_the_site.png" alt="" width='300px'/>
+                    <img src="/images/open_video_outside_the_site.png" alt="" width='330px'/>
                     <Typography>
                         Как и с альбомом - для просмотра длинного описания - нужно щелкнуть по нему один раз.
                     </Typography>
@@ -112,55 +110,56 @@ const Instructions = () => {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
-                        Все альбомы которые  альбомы google.
+                        Все альбомы используемые на этом сайте - созданы в приложении альбомы google.
                     </Typography>
                     <Typography>
-                        Подробную инструкцию о создании альбома можно найти тут по ссылке:
-                        <a
-                            href="https://support.google.com/photos/answer/6128849?hl=ru&co=GENIE.Platform%3DAndroid&oco=0"
-                            rel="noreferrer"
-                            target="_blank">
-                            Как создавать и редактировать фотоальбомы
-                        </a>
+                        Здесь можно ознакомиться с подробной инструкцией как создать альбом:
+                    </Typography>
+                    <a
+                        href="https://support.google.com/photos/answer/6128849?hl=ru&co=GENIE.Platform%3DAndroid&oco=0"
+                        rel="noreferrer"
+                        target="_blank">
+                        Как создавать и редактировать фотоальбомы
+                    </a>
+                    <Typography>
+                        После создания альбома - нужно открыть к нему доступ и поделиться ссылкой на него.
                     </Typography>
                 </AccordionDetails>
             </Accordion>
+            <Accordion
+                expanded={expanded === 'panel5'}
+                elevation={3}
+                onChange={handleChange('panel5')}
+            >
+                <AccordionSummary expandIcon={<ExpandMoreIcon/>} sx={{backgroundColor: "#e3f2fd"}}>
+                    <Typography sx={_title}>Новый видеоролик</Typography>
+                    <Typography sx={_subtitle}>Как добавить новый видеоролик на сайт?</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Все видеоролики используемые на этом сайте - созданы на сервисе youtube.
+                    </Typography>
+                    <Typography>
+                        Здесь можно ознакомиться с подробной инструкцией как создать видеоролик:
+                    </Typography>
+                    <a
+                        href="https://support.google.com/youtube/answer/57407?hl=ru&co=GENIE.Platform%3DDesktop"
+                        rel="noreferrer"
+                        target="_blank">
+                        Как загружать видео на YouTube
+                    </a>
+                    <Typography>
+                        После создания альбома - нужно открыть к нему доступ и поделиться ссылкой на него.
+                    </Typography>
+                    <Box>
+                        <img src="/images/share_youtube.png" alt="" width='330px'/>
+                    </Box>
+                    <Box>
+                        <img src="/images/embed_youtube.png" alt="" width='330px'/>
+                    </Box>
+                </AccordionDetails>
+            </Accordion>
         </Box>
-        // <Grid container spacing={2} sx={{p: 2}}>
-        //     <TextBlock
-        //         header="Общая информация"
-        //         text={[
-        //             "Сайт содержит альбомы (которые можно просмотреть только вне сайта) и видео-ролики (которые можно просмотреть как на самом сайте так и вне его).",
-        //             "Перемещаться по сайту можно с помощью меню слева выполнив щелчек по желаемому пункту меню. Некоторые пункты меню не открывают новой страницы, а содержат подпункты.",
-        //             "Если сайт просматривается на телефоне - то меню можно открыть выполнив щелчок в вернем левом углу на значке меню.",
-        //         ]}
-        //     />
-        //     <TextBlock
-        //         header="Как просматривать альбом?"
-        //         text={[
-        //             "Альбом содержит фотографию, заголовок и краткое описание.",
-        //             "Иногда описание альбома содержит больше текста, чем он может вместиться - для просмотра всего описания - нужно щелкнуть один раз по описанию. Чтобы вернуться к предыдущему размеру - щелкуть по описанию ещё один раз.",
-        //             "Чтобы начать просмотр альбома - щелкнуть по фотографии - при этом альбом откроется в новом окне (вне сайта) в прилоении альбомы google.",
-        //         ]}
-        //     />
-        //     <TextBlock
-        //         header="Как просматривать видеоролик?"
-        //         text={[
-        //             "Видеоролики (youtube) содержат в центре фотографии значок для воспроизведения, так же как и альбомы под видео находится заголовок и краткое описание.",
-        //             "Если щелкнуть по значку воспроизведения - можно начать просмотр в маленьком окне, двойной щелчок по видео или одинарный щелчок по значку полного экрана внизу справа - откроет ролик на весь экрна на этом сайте.",
-        //             "Если нужно открыть ролик вне сайта - нужно щелкнуть по заголовку ролика, который находится на самой картинке (не на заголовке что находится под картинкой).",
-        //             "Как и с альбомом - для того чтобы просмотреть длинное описание - нужно щелкнуть по нему один раз.",
-        //         ]}
-        //     />
-        //     <TextBlock
-        //         header="Как добавить новый альбом на сайт?"
-        //         text={[
-        //             "Удобнее всего добавить сюда альбомы google.",
-        //             "Подробную инструкцию можно найти тут по ссылке:"
-        //         ]}
-        //         link="https://support.google.com/photos/answer/6128849?hl=ru&co=GENIE.Platform%3DAndroid&oco=0"
-        //     />
-        // </Grid>
     );
 };
 
