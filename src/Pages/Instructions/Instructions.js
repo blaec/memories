200a037e9b8da29e1e18cc0e4e0a92afb69ba959
blaec <odeskonst@yandex.ru>
2023-09-28 21:58:34 +0300

    #11 refactoring

--- src/Pages/Instructions/Instructions.js
+++ src/Pages/Instructions/Instructions.js
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import {Box, Typography} from "@mui/material";
-import Accordion from '@mui/material/Accordion';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import InfoBlock from "./components/InfoBlock";
 
-const _title = {
-    width: '33%',
-    flexShrink: 0,
-    color: '#1976d2'
-};
-const _subtitle = {color: 'text.secondary'};
+import {Box, Typography} from "@mui/material";
+
+const _root = {p: 2};
+const imgWidth = '330px';
 
 
 const Instructions = () => {
@@ -21,144 +15,147 @@ const Instructions = () => {
         setExpanded(isExpanded ? panel : false);
     };
 
+    const generalInfo = (
+        <>
+            <Typography>
+                Сайт содержит альбомы (которые можно просмотреть только вне сайта)
+                и видео-ролики (которые можно просмотреть как на самом сайте так и вне его).
+            </Typography>
+            <Typography>
+                Перемещаться по сайту можно с помощью меню слева выполнив щелчек по желаемому пункту меню.
+                Некоторые пункты меню не открывают новой страницы, а содержат подпункты.
+            </Typography>
+            <Typography>
+                Если сайт просматривается на телефоне - то меню можно открыть выполнив щелчок в вернем левом
+                углу на значке меню.
+            </Typography>
+        </>
+    );
+    const album = (
+        <>
+            <Typography>
+                Карточка альбома содержит заглавную фотографию, заголовок и краткое описание.
+            </Typography>
+            <Typography>
+                Иногда описание альбома содержит больше текста, чем он может вместиться на карточке -
+                для просмотра всего описания нужно щелкнуть один раз по описанию.
+                Чтобы вернуться к предыдущему размеру карточки - щелкуть по описанию ещё один раз.
+            </Typography>
+            <Typography>
+                Для начала просмотра альбома - выполнить щелчок по заглавной фотографии -
+                при этом альбом откроется в новом окне (вне сайта) в приложении альбомы google.
+            </Typography>
+        </>
+    );
+    const video = (
+        <>
+            <Typography>
+                Видеоролики (youtube) содержат в центре фотографии значок для воспроизведения,
+                так же как и альбомы под видео располагается заголовок и краткое описание.
+            </Typography>
+            <Typography>
+                Если щелкнуть по значку воспроизведения - можно начать просмотр в маленьком окне,
+                двойной щелчок по видео или одинарный щелчок по значку полного экрана внизу справа -
+                откроет ролик на весь экран на этом сайте.
+            </Typography>
+            <Typography>
+                Для того, чтобы открыть ролик вне сайта - нужно щелкнуть по заголовку ролика как это показано
+                ниже.
+            </Typography>
+            <img src="/images/open_video_outside_the_site.png" alt="" width={imgWidth}/>
+            <Typography>
+                Как и с альбомом - для просмотра длинного описания - нужно щелкнуть по нему один раз.
+            </Typography>
+        </>
+    );
+    const newAlbum = (
+        <>
+            <Typography>
+                Все альбомы используемые на этом сайте - созданы в приложении альбомы google.
+            </Typography>
+            <Typography>
+                Здесь можно ознакомиться с подробной инструкцией как создать альбом:
+            </Typography>
+            <a
+                href="https://support.google.com/photos/answer/6128849?hl=ru&co=GENIE.Platform%3DAndroid&oco=0"
+                rel="noreferrer"
+                target="_blank">
+                Как создавать и редактировать фотоальбомы
+            </a>
+            <Typography>
+                После создания альбома - нужно открыть к нему доступ и поделиться ссылкой на него.
+            </Typography>
+        </>
+    );
+    const newVideo = (
+        <>
+            <Typography>
+                Все видеоролики используемые на этом сайте - созданы на сервисе youtube.
+            </Typography>
+            <Typography>
+                Здесь можно ознакомиться с подробной инструкцией как создать видеоролик:
+            </Typography>
+            <a
+                href="https://support.google.com/youtube/answer/57407?hl=ru&co=GENIE.Platform%3DDesktop"
+                rel="noreferrer"
+                target="_blank">
+                Как загружать видео на YouTube
+            </a>
+            <Typography>
+                После создания альбома - нужно открыть к нему доступ и поделиться ссылкой на него.
+            </Typography>
+            <Box>
+                <img src="/images/share_youtube.png" alt="" width={imgWidth}/>
+            </Box>
+            <Box>
+                <img src="/images/embed_youtube.png" alt="" width={imgWidth}/>
+            </Box>
+        </>
+    );
+
+
     return (
-        <Box sx={{p: 2}}>
-            <Accordion
-                expanded={expanded === 'panel1'}
-                elevation={3}
-                onChange={handleChange('panel1')}
-            >
-                <AccordionSummary expandIcon={<ExpandMoreIcon/>} sx={{backgroundColor: "#e3f2fd"}}>
-                    <Typography sx={_title}>Общая информация</Typography>
-                    <Typography sx={_subtitle}>Что есть на сайте?</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Сайт содержит альбомы (которые можно просмотреть только вне сайта)
-                        и видео-ролики (которые можно просмотреть как на самом сайте так и вне его).
-                    </Typography>
-                    <Typography>
-                        Перемещаться по сайту можно с помощью меню слева выполнив щелчек по желаемому пункту меню.
-                        Некоторые пункты меню не открывают новой страницы, а содержат подпункты.
-                    </Typography>
-                    <Typography>
-                        Если сайт просматривается на телефоне - то меню можно открыть выполнив щелчок в вернем левом
-                        углу на значке меню.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion
-                expanded={expanded === 'panel2'}
-                elevation={3}
-                onChange={handleChange('panel2')}
-            >
-                <AccordionSummary expandIcon={<ExpandMoreIcon/>} sx={{backgroundColor: "#e3f2fd"}}>
-                    <Typography sx={_title}>Фотоальбом</Typography>
-                    <Typography sx={_subtitle}>Как просмотреть альбом google?</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Карточка альбома содержит заглавную фотографию, заголовок и краткое описание.
-                    </Typography>
-                    <Typography>
-                        Иногда описание альбома содержит больше текста, чем он может вместиться на карточке -
-                        для просмотра всего описания нужно щелкнуть один раз по описанию.
-                        Чтобы вернуться к предыдущему размеру карточки - щелкуть по описанию ещё один раз.
-                    </Typography>
-                    <Typography>
-                        Для начала просмотра альбома - выполнить щелчок по заглавной фотографии -
-                        при этом альбом откроется в новом окне (вне сайта) в приложении альбомы google.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion
-                expanded={expanded === 'panel3'}
-                elevation={3}
-                onChange={handleChange('panel3')}
-            >
-                <AccordionSummary expandIcon={<ExpandMoreIcon/>} sx={{backgroundColor: "#e3f2fd"}}>
-                    <Typography sx={_title}>Видеоролик</Typography>
-                    <Typography sx={_subtitle}>Как просматривать видеоролик youtube?</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Видеоролики (youtube) содержат в центре фотографии значок для воспроизведения,
-                        так же как и альбомы под видео располагается заголовок и краткое описание.
-                    </Typography>
-                    <Typography>
-                        Если щелкнуть по значку воспроизведения - можно начать просмотр в маленьком окне,
-                        двойной щелчок по видео или одинарный щелчок по значку полного экрана внизу справа -
-                        откроет ролик на весь экран на этом сайте.
-                    </Typography>
-                    <Typography>
-                        Для того, чтобы открыть ролик вне сайта - нужно щелкнуть по заголовку ролика как это показано ниже.
-                    </Typography>
-                    <img src="/images/open_video_outside_the_site.png" alt="" width='330px'/>
-                    <Typography>
-                        Как и с альбомом - для просмотра длинного описания - нужно щелкнуть по нему один раз.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion
-                expanded={expanded === 'panel4'}
-                elevation={3}
-                onChange={handleChange('panel4')}
-            >
-                <AccordionSummary expandIcon={<ExpandMoreIcon/>} sx={{backgroundColor: "#e3f2fd"}}>
-                    <Typography sx={_title}>Новый фотоальбом</Typography>
-                    <Typography sx={_subtitle}>Как добавить новый альбом на сайт?</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Все альбомы используемые на этом сайте - созданы в приложении альбомы google.
-                    </Typography>
-                    <Typography>
-                        Здесь можно ознакомиться с подробной инструкцией как создать альбом:
-                    </Typography>
-                    <a
-                        href="https://support.google.com/photos/answer/6128849?hl=ru&co=GENIE.Platform%3DAndroid&oco=0"
-                        rel="noreferrer"
-                        target="_blank">
-                        Как создавать и редактировать фотоальбомы
-                    </a>
-                    <Typography>
-                        После создания альбома - нужно открыть к нему доступ и поделиться ссылкой на него.
-                    </Typography>
-                </AccordionDetails>
-            </Accordion>
-            <Accordion
-                expanded={expanded === 'panel5'}
-                elevation={3}
-                onChange={handleChange('panel5')}
-            >
-                <AccordionSummary expandIcon={<ExpandMoreIcon/>} sx={{backgroundColor: "#e3f2fd"}}>
-                    <Typography sx={_title}>Новый видеоролик</Typography>
-                    <Typography sx={_subtitle}>Как добавить новый видеоролик на сайт?</Typography>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>
-                        Все видеоролики используемые на этом сайте - созданы на сервисе youtube.
-                    </Typography>
-                    <Typography>
-                        Здесь можно ознакомиться с подробной инструкцией как создать видеоролик:
-                    </Typography>
-                    <a
-                        href="https://support.google.com/youtube/answer/57407?hl=ru&co=GENIE.Platform%3DDesktop"
-                        rel="noreferrer"
-                        target="_blank">
-                        Как загружать видео на YouTube
-                    </a>
-                    <Typography>
-                        После создания альбома - нужно открыть к нему доступ и поделиться ссылкой на него.
-                    </Typography>
-                    <Box>
-                        <img src="/images/share_youtube.png" alt="" width='330px'/>
-                    </Box>
-                    <Box>
-                        <img src="/images/embed_youtube.png" alt="" width='330px'/>
-                    </Box>
-                </AccordionDetails>
-            </Accordion>
+        <Box sx={_root}>
+            <InfoBlock
+                id="panel1"
+                title="Общая информация"
+                subtitle="Что есть на сайте?"
+                info={generalInfo}
+                expanded={expanded}
+                onChange={handleChange}
+            />
+            <InfoBlock
+                id="panel2"
+                title="Фотоальбом"
+                subtitle="Как просмотреть альбом google?"
+                info={album}
+                expanded={expanded}
+                onChange={handleChange}
+            />
+            <InfoBlock
+                id="panel3"
+                title="Видеоролик"
+                subtitle="Как просматривать видеоролик youtube?"
+                info={video}
+                expanded={expanded}
+                onChange={handleChange}
+            />
+            <InfoBlock
+                id="panel4"
+                title="Новый фотоальбом"
+                subtitle="Как добавить новый альбом на сайт?"
+                info={newAlbum}
+                expanded={expanded}
+                onChange={handleChange}
+            />
+            <InfoBlock
+                id="panel5"
+                title="Новый видеоролик"
+                subtitle="Как добавить новый видеоролик на сайт?"
+                info={newVideo}
+                expanded={expanded}
+                onChange={handleChange}
+            />
         </Box>
     );
 };
